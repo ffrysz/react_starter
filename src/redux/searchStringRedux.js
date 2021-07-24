3519969a8b7bcb91d87bfdf4b60adcdeb3d2c3ba
Filip Frysz <ffrysz@gmail.com>
2021-07-24 23:08:05 +0200

    Add search

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,31 @@
+
+// selectors
+export const getSearchString = (state) => state.searchString;
+
+export const countVisibleCards = ({ cards, searchString }) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+
+export const countAllCards = ({ cards }) => cards.length;
+
+// action name creator
+
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+
+export const SEARCH_CARD = createActionName('SEARCH_CARD');
+
+// action creators
+
+export const createAction_changeSearchString = payload => ({ payload, type: SEARCH_CARD });
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case SEARCH_CARD:
+      return [action.payload];
+    default:
+      return statePart;
+  }
+}
+
